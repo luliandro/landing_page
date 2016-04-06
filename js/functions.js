6ef6f63df3f120390f24d1bd1e56a37347c17ed0
Mat Griffiths <mat@hevnly.com>
2016-04-06 16:13:47 +0100

    tidy up and delete legacy JS that isn't used anymore

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,3 @@
-function setupOffCanvasNav(jsonUser){
-    if (jsonUser.is_app_user) {
-        $("span.navicon").addClass("logged-in-appuser");
-    }
-    if (jsonUser.profile_pic != undefined) {
-        $("#profile-picture").attr("src", $(document).data("profilePicBaseUrl") + jsonUser.profile_pic.path);
-    }
-}
-
 //add deprecated jquery toggle function
 jQuery.fn.toggle = function( fn, fn2 ) {
   // Don't mess with animation or css toggles
@@ -34,3 +25,47 @@ jQuery.fn.toggle = function( fn, fn2 ) {
   }
   return this.click( toggler );
 };
+
+
+jQuery(document).ready(function() {
+
+    //stop iOS zoom on form fields - from https://gist.github.com/zachleat/2008932
+        function cancelZoom()
+    {
+        var d = document,
+            viewport,
+            content,
+            maxScale = ',maximum-scale=',
+            maxScaleRegex = /,*maximum\-scale\=\d*\.*\d*/;
+
+        // this should be a focusable DOM Element
+        if(!this.addEventListener || !d.querySelector) {
+            return;
+        }
+
+        viewport = d.querySelector('meta[name="viewport"]');
+        content = viewport.content;
+
+        function changeViewport(event)
+        {
+            // http://nerd.vasilis.nl/prevent-ios-from-zooming-onfocus/
+            viewport.content = content + (event.type === 'blur' ? (content.match(maxScaleRegex, '') ? '' : maxScale + 10) : maxScale + 1);
+        }
+
+        // We could use DOMFocusIn here, but it's deprecated.
+        this.addEventListener('focus', changeViewport, true);
+        this.addEventListener('blur', changeViewport, false);
+    }
+
+    // jQuery-plugin
+    (function($)
+    {
+        $.fn.cancelZoom = function()
+        {
+            return this.each(cancelZoom);
+        };
+
+        // Usage:
+        $('input,select,textarea').cancelZoom();
+    })(jQuery);
+});
